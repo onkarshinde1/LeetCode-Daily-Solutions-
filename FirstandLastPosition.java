b031421fc9885a120e42b8a3dbb6a897afeca20c
Onkar Shinde <onkarshinde499@gmail.com>
2026-01-01 22:12:55 +0530

    34. Find First and Last Position of Element in Sorted Array

--- FirstandLastPosition.java
+++ FirstandLastPosition.java
@@ -1,7 +1,28 @@
 public class FirstandLastPosition {
+    // 34. Find First and Last Position of Element in Sorted Array
+
+    // Given an array of integers nums sorted in non-decreasing order, find the
+    // starting and ending position of a given target value.
+
+    // If target is not found in the array, return [-1, -1].
+
+    // You must write an algorithm with O(log n) runtime complexity.
+
+    // Example 1:
+
+    // Input: nums = [5,7,7,8,8,10], target = 8
+    // Output: [3,4]
+    // Example 2:
+
+    // Input: nums = [5,7,7,8,8,10], target = 6
+    // Output: [-1,-1]
+    // Example 3:
+
+    // Input: nums = [], target = 0
+    // Output: [-1,-1]
 
     public static void main(String[] args) {
-        int[] arr = {5,6,7,7,7,7,7,8,8,10,12};
+        int[] arr = { 5, 6, 7, 7, 7, 7, 7, 8, 8, 10, 12 };
         int target = 7;
 
         FirstandLastPosition obj = new FirstandLastPosition();
@@ -12,7 +33,7 @@ public class FirstandLastPosition {
     }
 
     public int[] searchRange(int[] arr, int target) {
-        int[] result = {-1, -1};
+        int[] result = { -1, -1 };
 
         result[0] = findFirst(arr, target);
         result[1] = findLast(arr, target);
@@ -29,8 +50,8 @@ public class FirstandLastPosition {
             int mid = start + (end - start) / 2;
 
             if (arr[mid] == target) {
-                ans = mid;      // store answer
-                end = mid - 1;  // move left
+                ans = mid; // store answer
+                end = mid - 1; // move left
             } else if (arr[mid] < target) {
                 start = mid + 1;
             } else {
@@ -49,7 +70,7 @@ public class FirstandLastPosition {
             int mid = start + (end - start) / 2;
 
             if (arr[mid] == target) {
-                ans = mid;      // store answer
+                ans = mid; // store answer
                 start = mid + 1; // move right
             } else if (arr[mid] < target) {
                 start = mid + 1;
